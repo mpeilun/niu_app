2b0e63e7a3de563718d0b4d6597e0da33d4e9e8f
changpeilun <peilun2016@gmail.com>
2021-07-09 19:37:28 +0800

    login with route reedit

--- lib/main.dart
+++ lib/main.dart
@@ -8,9 +8,7 @@ Future main() async {
 
   await Permission.storage.request();
 
-  runApp(new MaterialApp(
-    home: MyApp()
-  ));
+  runApp(new MaterialApp(home: MyApp()));
 }
 
 class MyApp extends StatefulWidget {
@@ -19,7 +17,6 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-
   @override
   void initState() {
     super.initState();
@@ -33,42 +30,138 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-          appBar: AppBar(title: Text("功能列表")),
-          body: SafeArea(
-              child: Column(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: <Widget>[
-                    RaisedButton(onPressed: () {
-                      Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) => LoginPage(), maintainState: false));
-                    },),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Icon(Icons.access_alarm,size: 50, color: Colors.amber,),
-                    Icon(Icons.access_alarm,size: 50, color: Colors.amber,),
-                    Icon(Icons.access_alarm,size: 50, color: Colors.amber,),
-                  ],
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Icon(Icons.access_alarm,size: 50, color: Colors.amber,),
-                    Icon(Icons.access_alarm,size: 50, color: Colors.amber,),
-                    Icon(Icons.access_alarm,size: 50, color: Colors.amber,),
-                  ],
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Icon(Icons.access_alarm,size: 50, color: Colors.amber,),
-                    Icon(Icons.access_alarm,size: 50, color: Colors.amber,),
-                    Icon(Icons.access_alarm,size: 50, color: Colors.amber,),
-                  ],
-                ),
+      appBar: AppBar(title: Text("功能列表")),
+      body: SafeArea(
+          child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+            SizedBox(height: 60),
+            Container(
+              padding: EdgeInsets.all(10),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Column(children: <Widget>[
+                    IconButton(
+                      icon: Icon(Icons.login, size: 50, color: Colors.amber),
+                      onPressed: () {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => LoginPage(),
+                                maintainState: false));
+                      },
+                    ),
+                    Text('登入')
+                  ]),
+                  SizedBox(
+                    width: 50,
+                  ),
+                  Column(children: <Widget>[
+                    Icon(
+                      Icons.two_k,
+                      size: 50,
+                      color: Colors.amber,
+                    ),
+                    Text('成績')
+                  ]),
+                  SizedBox(
+                    width: 50,
+                  ),
+                  Column(children: <Widget>[
+                    Icon(
+                      Icons.thirteen_mp,
+                      size: 50,
+                      color: Colors.amber,
+                    ),
+                    Text('課表')
+                  ]),
+                ],
+              ),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Container(
+              padding: EdgeInsets.all(10),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Column(children: <Widget>[
+                    Icon(
+                      Icons.access_alarm,
+                      size: 50,
+                      color: Colors.amber,
+                    ),
+                    Text('活動報名')
+                  ]),
+                  SizedBox(
+                    width: 50,
+                  ),
+                  Column(children: <Widget>[
+                    Icon(
+                      Icons.access_alarm,
+                      size: 50,
+                      color: Colors.amber,
+                    ),
+                    Text('ZUVIO')
+                  ]),
+                  SizedBox(
+                    width: 50,
+                  ),
+                  Column(children: <Widget>[
+                    Icon(
+                      Icons.access_alarm,
+                      size: 50,
+                      color: Colors.amber,
+                    ),
+                    Text('畢業門檻')
+                  ]),
+                ],
+              ),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Container(
+              padding: EdgeInsets.all(10),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Column(children: <Widget>[
+                    Icon(
+                      Icons.access_alarm,
+                      size: 50,
+                      color: Colors.amber,
+                    ),
+                    Text('選課系統')
+                  ]),
+                  SizedBox(
+                    width: 50,
+                  ),
+                  Column(children: <Widget>[
+                    Icon(
+                      Icons.access_alarm,
+                      size: 50,
+                      color: Colors.amber,
+                    ),
+                    Text('公車')
+                  ]),
+                  SizedBox(
+                    width: 50,
+                  ),
+                  Column(children: <Widget>[
+                    Icon(
+                      Icons.access_alarm,
+                      size: 50,
+                      color: Colors.amber,
+                    ),
+                    Text('帳號設定')
+                  ]),
+                ],
+              ),
+            ),
           ])),
     );
   }
