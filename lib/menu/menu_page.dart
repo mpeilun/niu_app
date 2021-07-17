555ddb6a1d66661592aaa6ad7cd14adbe2b6f9cb
changpeilun <peilun2016@gmail.com>
2021-07-17 22:22:34 +0800

    進度提醒器

--- lib/menu/menu_page.dart
+++ lib/menu/menu_page.dart
@@ -10,6 +10,7 @@ import 'package:niu_app/testwebview_headless.dart';
 import 'package:niu_app/TimeTable/TimeTable.dart';
 
 import 'package:niu_app/service/SemesterDate.dart';
+import 'package:percent_indicator/linear_percent_indicator.dart';
 
 import '../testwebview.dart';
 
@@ -36,6 +37,8 @@ class _StartMenu extends State<StartMenu> {
   @override
   Widget build(BuildContext context) {
     //Size size = MediaQuery.of(context).size;
+    int day = daysBetween(DateTime.now(), DateTime(2021, 9, 13));
+    double goal = 1 - day / 77.0;
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
@@ -52,6 +55,30 @@ class _StartMenu extends State<StartMenu> {
       ),
       body: SafeArea(
           child: Column(children: [
+        Image.network(
+            'https://thumbs.gfycat.com/EminentRecentAplomadofalcon-max-1mb.gif'),
+        SizedBox(height: 10),
+        LinearPercentIndicator(
+          alignment: MainAxisAlignment.center,
+          width: 200.0,
+          animation: true,
+          animationDuration: 5000,
+          lineHeight: 25,
+          leading:
+              Text("暑假開始", style: TextStyle(color: Colors.green, fontSize: 18)),
+          trailing:
+              Text("暑假結束", style: TextStyle(color: Colors.red, fontSize: 18)),
+          percent: goal,
+          center: Text((goal * 100).toStringAsFixed(1),
+              style: TextStyle(color: Colors.blue, fontSize: 16)),
+          linearStrokeCap: LinearStrokeCap.butt,
+          progressColor: Colors.amber,
+        ),
+        SizedBox(
+          height: 10,
+        ),
+        Text("剩下$day天",
+            style: TextStyle(color: Colors.pinkAccent, fontSize: 18)),
         Expanded(
           flex: 6,
           child: Column(mainAxisAlignment: MainAxisAlignment.spaceEvenly,
@@ -110,8 +137,8 @@ class _StartMenu extends State<StartMenu> {
                             context,
                             MaterialPageRoute(
                                 builder: (context) => SchoolEvent(
-                                  title: '活動報名',
-                                ),
+                                      title: '活動報名',
+                                    ),
                                 maintainState: false));
                       },
                     ),
@@ -161,8 +188,14 @@ class _StartMenu extends State<StartMenu> {
                 ),
               ]),
         ),
-        Expanded(flex: 4, child: Image.asset('assets/niu_background.png'))
+        //Expanded(flex: 4, child: Image.asset('assets/niu_background.png'))
       ])),
     );
+  }
+
+  int daysBetween(DateTime from, DateTime to) {
+    from = DateTime(from.year, from.month, from.day);
+    to = DateTime(to.year, to.month, to.day);
+    return (to.difference(from).inHours / 24).round();
   }
 }
