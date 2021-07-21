77b460793872e60d5699e89edf901be4e5d2ff8e
changpeilun <peilun2016@gmail.com>
2021-07-22 01:56:30 +0800

    fix 登入頁面卡住

--- lib/login/login_page.dart
+++ lib/login/login_page.dart
@@ -15,7 +15,7 @@ class _LoginPageState extends State<LoginPage> {
   final TextEditingController _controllerID = TextEditingController();
   final TextEditingController _controllerPWD = TextEditingController();
   HeadlessInAppWebView? headlessWebView;
-  bool loadState = true;
+  bool loadState = false;
   String url = "";
   String id = "";
   String pwd = "";
@@ -25,42 +25,55 @@ class _LoginPageState extends State<LoginPage> {
     super.initState();
 
     headlessWebView = new HeadlessInAppWebView(
-      initialUrlRequest: URLRequest(
-          url: Uri.parse("https://acade.niu.edu.tw/NIU/logout.aspx")),
-      initialOptions: InAppWebViewGroupOptions(
-        crossPlatform: InAppWebViewOptions(
-          javaScriptCanOpenWindowsAutomatically: true,
+        initialUrlRequest: URLRequest(
+            url: Uri.parse("https://acade.niu.edu.tw/NIU/logout.aspx")),
+        initialOptions: InAppWebViewGroupOptions(
+          crossPlatform: InAppWebViewOptions(
+            javaScriptCanOpenWindowsAutomatically: true,
+          ),
         ),
-      ),
-      onWebViewCreated: (controller) {
-        print('HeadlessInAppWebView created!');
-      },
-      onConsoleMessage: (controller, consoleMessage) {
-        print("CONSOLE MESSAGE: " + consoleMessage.message);
-      },
-      onLoadStart: (controller, url) async {
-        print("onLoadStart $url");
-        setState(() {
-          this.url = url.toString();
+        onWebViewCreated: (controller) {
+          print('HeadlessInAppWebView created!');
+        },
+        onConsoleMessage: (controller, consoleMessage) {
+          print("CONSOLE MESSAGE: " + consoleMessage.message);
+        },
+        onLoadStart: (controller, url) async {
+          print("onLoadStart $url");
+          setState(() {
+            this.url = url.toString();
+          });
+        },
+        onLoadStop: (controller, url) async {
+          print("onLoadStop $url");
+          setState(() {
+            this.url = url.toString();
+          });
+          if (url.toString() == 'https://acade.niu.edu.tw/NIU/Default.aspx') {
+            setState(() {
+              loadState = true;
+            });
+          }
+          if (url.toString() == 'https://acade.niu.edu.tw/NIU/MainFrame.aspx') {
+            print('登入成功');
+            await _saveData(id, pwd);
+          }
+        },
+        onUpdateVisitedHistory: (controller, url, androidIsReload) {
+          print("onUpdateVisitedHistory $url");
+          setState(() {
+            this.url = url.toString();
+          });
+        },
+        onJsAlert: (InAppWebViewController controller,
+            JsAlertRequest jsAlertRequest) async {
+          await headlessWebView?.webViewController.loadUrl(
+              urlRequest: URLRequest(
+                  url: Uri.parse("https://acade.niu.edu.tw/NIU/Default.aspx")));
+          JsAlertResponseAction action =
+              await createAlertDialog(context, '帳號或密碼錯誤，請查明後再登入!');
+          return JsAlertResponse(handledByClient: true, action: action);
         });
-      },
-      onLoadStop: (controller, url) async {
-        print("onLoadStop $url");
-        setState(() {
-          this.url = url.toString();
-        });
-        if (url.toString() == 'https://acade.niu.edu.tw/NIU/MainFrame.aspx') {
-          print('登入成功');
-          await _saveData(id, pwd);
-        }
-      },
-      onUpdateVisitedHistory: (controller, url, androidIsReload) {
-        print("onUpdateVisitedHistory $url");
-        setState(() {
-          this.url = url.toString();
-        });
-      },
-    );
 
     headlessWebView?.run();
   }
@@ -80,57 +93,75 @@ class _LoginPageState extends State<LoginPage> {
             title: Text(widget.title),
             centerTitle: true,
             automaticallyImplyLeading: false),
-        body: SingleChildScrollView(
-          child: Column(
-            children: <Widget>[
-              Container(
-                padding: EdgeInsets.symmetric(horizontal: 24.0, vertical: 16.0),
-                child: TextFormField(
-                  controller: _controllerID,
-                  onChanged: (String value) async {
-                    id = value;
-                  },
-                  decoration: const InputDecoration(
-                    prefixIcon: Icon(Icons.person),
-                    labelText: "學號 *",
-                    hintText: "輸入您的的學號",
+        body: Column(
+          children: <Widget>[
+            Visibility(
+              visible: !loadState,
+              child: Expanded(
+                child: Image.network(
+                    'https://i.pinimg.com/originals/6b/67/cb/6b67cb8a166c0571c1290f205c513321.gif'),
+              ),
+            ),
+            Visibility(
+              visible: loadState,
+              child: Expanded(
+                child: SingleChildScrollView(
+                  child: Column(
+                    children: <Widget>[
+                      Container(
+                        padding: EdgeInsets.symmetric(
+                            horizontal: 24.0, vertical: 16.0),
+                        child: TextFormField(
+                          controller: _controllerID,
+                          onChanged: (String value) async {
+                            id = value;
+                          },
+                          decoration: const InputDecoration(
+                            prefixIcon: Icon(Icons.person),
+                            labelText: "學號 *",
+                            hintText: "輸入您的的學號",
+                          ),
+                        ),
+                      ),
+                      Container(
+                        padding: EdgeInsets.symmetric(
+                            horizontal: 24.0, vertical: 16.0),
+                        child: TextFormField(
+                          controller: _controllerPWD,
+                          onChanged: (String value) async {
+                            pwd = value;
+                          },
+                          obscureText: true,
+                          decoration: const InputDecoration(
+                            prefixIcon: Icon(Icons.lock),
+                            suffixIcon: Icon(Icons.remove_red_eye),
+                            labelText: "密碼 *",
+                            hintText: "預設身分證前八碼",
+                          ),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 52.0,
+                      ),
+                      SizedBox(
+                        width: MediaQuery.of(context).size.width - 48.0,
+                        height: 48.0,
+                        child: Visibility(
+                          visible: loadState,
+                          child: ElevatedButton(
+                            child: Text("登入"),
+                            onPressed: () async {
+                              await login();
+                            },
+                          ),
+                        ),
+                      ),
+                    ],
                   ),
                 ),
               ),
-              Container(
-                padding: EdgeInsets.symmetric(horizontal: 24.0, vertical: 16.0),
-                child: TextFormField(
-                  controller: _controllerPWD,
-                  onChanged: (String value) async {
-                    pwd = value;
-                  },
-                  obscureText: true,
-                  decoration: const InputDecoration(
-                    prefixIcon: Icon(Icons.lock),
-                    suffixIcon: Icon(Icons.remove_red_eye),
-                    labelText: "密碼 *",
-                    hintText: "預設身分證前八碼",
-                  ),
-                ),
-              ),
-              SizedBox(
-                height: 52.0,
-              ),
-              SizedBox(
-                width: MediaQuery.of(context).size.width - 48.0,
-                height: 48.0,
-                child: Visibility(
-                  visible: loadState,
-                  child: ElevatedButton(
-                    child: Text("登入"),
-                    onPressed: () async {
-                      await login();
-                    },
-                  ),
-                ),
-              ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
@@ -149,11 +180,6 @@ class _LoginPageState extends State<LoginPage> {
       await headlessWebView?.webViewController.evaluateJavascript(
           source: 'document.querySelector("#LGOIN_BTN").click();');
     });
-    Future.delayed(Duration(seconds: 5), () async {
-      setState(() {
-        loadState = true;
-      });
-    });
   }
 
   _saveData(String id, String pwd) async {
@@ -161,5 +187,31 @@ class _LoginPageState extends State<LoginPage> {
     prefs.setString("id", id);
     prefs.setString("pwd", pwd);
     Navigator.pop(context);
+  }
+
+  Future<JsAlertResponseAction> createAlertDialog(
+      BuildContext context, String message) async {
+    late JsAlertResponseAction action;
+
+    await showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          content: Text(message),
+          actions: <Widget>[
+            TextButton(
+              child: Text("Ok"),
+              key: Key("AlertButtonOk"),
+              onPressed: () {
+                action = JsAlertResponseAction.CONFIRM;
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
+
+    return action;
   }
 }
