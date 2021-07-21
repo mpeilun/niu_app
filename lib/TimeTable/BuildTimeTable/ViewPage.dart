78da274910f99014a4baa9902c1a725074bda59c
周楷崴 <ken60786213@gmail.com>
2021-07-22 01:46:57 +0800

    小小的進度

--- lib/TimeTable/BuildTimeTable/ViewPage.dart
+++ lib/TimeTable/BuildTimeTable/ViewPage.dart
@@ -5,7 +5,10 @@ import 'ClassList.dart';
 import 'Class.dart';
 
 class ViewPage extends StatelessWidget {
-  const ViewPage.build();
+  const ViewPage.build({
+    required this.myTable,
+  });
+  Class myTable;
 
   @override
   Widget build(BuildContext context) {
@@ -31,18 +34,3 @@ class ViewPage extends StatelessWidget {
     );
   }
 }
-
-
-List<Class> myTable = <Class>[
-  Class("電子電路","朱志明","教416",1,2,4),
-  Class("工程數學","黃朝曦","教102",1,5,5),
-  Class("英文聽講","待聘教師","綜105",1,7,8),
-  Class("人工智慧導論","林斯寅","格406",1,9,9),
-  Class("GC服務管理","黃麗君","教214",2,3,4),
-  Class("人工智慧導論","林斯寅","格406",2,5,6),
-  Class("工程數學","黃朝曦","教102",3,3,4),
-  Class("離散數學","吳汶涓","綜202",3,7,9),
-  Class("GB台灣開發史","陳政吉","教105",3,10,11),
-  Class("資訊安全導論","陳麒元","格406",4,5,7),
-  Class("微處理器系統","卓信宏","教416",5,2,4)
-];
