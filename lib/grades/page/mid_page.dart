d2196fef7569cdc79989fa5f7afb26e290926e09
changpeilun <peilun2016@gmail.com>
2021-08-01 01:05:55 +0800

    預時

--- lib/grades/page/mid_page.dart
+++ lib/grades/page/mid_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_inappwebview/flutter_inappwebview.dart';
 import 'package:niu_app/components/niu_icon_loading.dart';
 import 'package:niu_app/grades/custom_cards.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class MidPage extends StatefulWidget {
   const MidPage({Key? key}) : super(key: key);
@@ -48,46 +49,78 @@ class _MidPageState extends State<MidPage>
           setState(() {
             this.url = url.toString();
           });
+          if (url.toString() == 'https://acade.niu.edu.tw/NIU/Default.aspx') {
+            setState(() {
+              loadStates = false;
+            });
+          }
         },
         onLoadStop: (controller, url) async {
           print("onLoadStop $url");
           setState(() {
             this.url = url.toString();
           });
-          for (int i = 2; //2~N
-              (await controller.evaluateJavascript(
-                          source:
-                              'document.querySelector("#DataGrid > tbody > tr:nth-child($i)").innerHTML'))
-                      .toString() !=
-                  'null';
-              i++) {
-            String type = await controller.evaluateJavascript(
+          if (url.toString() == 'https://acade.niu.edu.tw/NIU/Default.aspx') {
+            SharedPreferences prefs = await SharedPreferences.getInstance();
+            String? id = prefs.getString('id');
+            String? pwd = prefs.getString('pwd');
+            await headlessWebView?.webViewController.evaluateJavascript(
                 source:
-                    'document.querySelector("#DataGrid > tbody > tr:nth-child($i) > td:nth-child(4)").innerText');
-            String lesson = await controller.evaluateJavascript(
-                source:
-                    'document.querySelector("#DataGrid > tbody > tr:nth-child($i) > td:nth-child(5)").innerText');
-            String score = await controller.evaluateJavascript(
-                source:
-                    'document.querySelector("#DataGrid > tbody > tr:nth-child($i) > td:nth-child(6)").innerText');
-            if (type.length < 2) {
-              type = '必修';
-            }
-            grades.add(Quote(lesson: lesson, score: score, type: type));
+                    'document.querySelector("#M_PORTAL_LOGIN_ACNT").value=\'$id\';');
+            await headlessWebView?.webViewController.evaluateJavascript(
+                source: 'document.querySelector("#M_PW").value=\'$pwd\';');
+            Future.delayed(Duration(milliseconds: 1000), () async {
+              await headlessWebView?.webViewController.evaluateJavascript(
+                  source: 'document.querySelector("#LGOIN_BTN").click();');
+            });
           }
-          grades.sort((a, b) {
-            return a.score!.compareTo(b.score!);
-          });
-          grades.sort((a, b) {
-            if (a.score!.contains('未上傳') || b.score!.contains('未上傳')) {
-              return -1;
-            } else {
-              return double.parse(b.score!).compareTo(double.parse(a.score!));
+          if (url.toString() == 'https://acade.niu.edu.tw/NIU/MainFrame.aspx') {
+            await headlessWebView?.webViewController.loadUrl(
+                urlRequest: URLRequest(
+                    url: Uri.parse(
+                        "https://acade.niu.edu.tw/NIU/Application/GRD/GRD51/GRD5131_02.aspx"),
+                    headers: {
+                  "Referer":
+                      "https://acade.niu.edu.tw/NIU/Application/GRD/GRD51/GRD5131_.aspx?progcd=GRD5131"
+                }));
+          }
+          if (url.toString() ==
+              'https://acade.niu.edu.tw/NIU/Application/GRD/GRD51/GRD5131_02.aspx') {
+            for (int i = 2; //2~N
+                (await controller.evaluateJavascript(
+                            source:
+                                'document.querySelector("#DataGrid > tbody > tr:nth-child($i)").innerHTML'))
+                        .toString() !=
+                    'null';
+                i++) {
+              String type = await controller.evaluateJavascript(
+                  source:
+                      'document.querySelector("#DataGrid > tbody > tr:nth-child($i) > td:nth-child(4)").innerText');
+              String lesson = await controller.evaluateJavascript(
+                  source:
+                      'document.querySelector("#DataGrid > tbody > tr:nth-child($i) > td:nth-child(5)").innerText');
+              String score = await controller.evaluateJavascript(
+                  source:
+                      'document.querySelector("#DataGrid > tbody > tr:nth-child($i) > td:nth-child(6)").innerText');
+              if (type.length < 2) {
+                type = '必修';
+              }
+              grades.add(Quote(lesson: lesson, score: score, type: type));
             }
-          });
-          setState(() {
-            loadStates = true;
-          });
+            grades.sort((a, b) {
+              return a.score!.compareTo(b.score!);
+            });
+            grades.sort((a, b) {
+              if (a.score!.contains('未上傳') || b.score!.contains('未上傳')) {
+                return -1;
+              } else {
+                return double.parse(b.score!).compareTo(double.parse(a.score!));
+              }
+            });
+            setState(() {
+              loadStates = true;
+            });
+          }
         },
         onUpdateVisitedHistory: (controller, url, androidIsReload) {
           print("onUpdateVisitedHistory $url");
@@ -102,7 +135,13 @@ class _MidPageState extends State<MidPage>
         },
         onJsAlert: (InAppWebViewController controller,
             JsAlertRequest jsAlertRequest) async {
-          print('onJsAlert ' + jsAlertRequest.message.toString());
+          print(jsAlertRequest.message.toString());
+          if (jsAlertRequest.message.toString().contains('使用時間逾時')) {
+            await headlessWebView?.webViewController.loadUrl(
+                urlRequest: URLRequest(
+                    url: Uri.parse(
+                        "https://acade.niu.edu.tw/NIU/Default.aspx")));
+          }
           return JsAlertResponse(
               handledByClient: true, action: JsAlertResponseAction.CONFIRM);
         },
