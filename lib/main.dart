304a151d88eaf0d581cf96de24d089afdafd7d82
changpeilun <peilun2016@gmail.com>
2021-10-05 01:23:43 +0800

    fix

--- lib/main.dart
+++ lib/main.dart
@@ -8,15 +8,17 @@ import 'package:niu_app/provider/drawer_provider.dart';
 import 'package:oktoast/oktoast.dart';
 import 'package:provider/provider.dart';
 
+import 'TimeTable/BuildTimeTable/button_provider.dart';
+
 void main() {
   runApp(MultiProvider(providers: [
     ChangeNotifierProvider(create: (_) => OnItemClick()),
+    ChangeNotifierProvider(create: (_) => OnTimeCardClick()),
   ], child: MyApp()));
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
     final textTheme = Theme.of(context).textTheme;
