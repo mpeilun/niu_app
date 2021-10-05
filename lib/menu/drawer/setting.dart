0f4a420c0d771c3252d669f16a8968f24cd5b815
changpeilun <peilun2016@gmail.com>
2021-10-06 04:20:01 +0800

    行事曆通知欠缺測試

--- lib/menu/drawer/setting.dart
+++ lib/menu/drawer/setting.dart
@@ -1,9 +1,54 @@
 import 'package:flutter/material.dart';
+import 'package:dio/dio.dart';
+import 'package:html/dom.dart' as prefix;
+import 'package:html/dom_parsing.dart';
+import 'package:html/parser.dart';
 
-class SettingPage extends StatelessWidget {
+import 'package:niu_app/components/niu_icon_loading.dart';
+import 'package:niu_app/service/CalendarMessage.dart';
+import 'package:niu_app/service/SemesterDate.dart';
+
+class SettingPage extends StatefulWidget {
+  @override
+  State<SettingPage> createState() => _SettingPageState();
+}
+
+class _SettingPageState extends State<SettingPage> {
+  List<CalenderMessageType> calenderMsg = [];
+  SemesterDate a = SemesterDate();
+  CalenderMessage calenderMessage = new CalenderMessage();
+  Future<bool> isFinish() async {
+    calenderMsg = await calenderMessage.getList();
+    await a.getIsFinish();
+    return true;
+  }
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Center(child: Text("Setting"));
+    return Container(
+      child: Scaffold(
+        body: FutureBuilder(
+            future: isFinish(),
+            builder: (BuildContext context, AsyncSnapshot snap) {
+              calenderMsg.forEach((element) {
+                print(element.calendar.name() +
+                    element.date(a).toString() +
+                    element.calendar.save());
+              });
+
+              return calenderMessage.set(context, calenderMsg.first);
+            }),
+      ),
+    );
   }
 }
