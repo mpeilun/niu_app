b1cacf90049f235f4351f4dd3eded8044f29dd86
mpeilun <peilun2016@gmail.com>
2021-11-30 19:34:10 +0800

    fix

--- lib/menu/statisfaction_survey/form.dart
+++ lib/menu/statisfaction_survey/form.dart
@@ -1,20 +1,33 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:rflutter_alert/rflutter_alert.dart';
 
 void toDoFormAlert(BuildContext context) {
-  final imageProvider = NetworkImage('');
   Alert(
-    image: Container(
-      child: Image.asset('assets/satisfaction.png'),
-    ),
     context: context,
-    title: "是否願意幫我們填寫意願表單呢？",
-    content: SizedBox(
-      child: Text(
-        ' 填寫完成將解鎖黑色主題哦！',
-        style: TextStyle(color: Colors.grey.shade800, fontSize: 16),
-      ),
+    type: AlertType.info,
+    image: SizedBox(
+      height: 150,
+      width: 150,
+      child: Image.asset('assets/satisfaction_black.png'),
+    ),
+    content: Column(
+      children: [
+        Text(
+          '是否願意幫我們填寫意願表單呢？',
+          style: TextStyle(
+              color: Colors.black, fontSize: 18, fontWeight: FontWeight.w200),
+          textAlign: TextAlign.center,
+        ),
+        SizedBox(
+          height: 10,
+        ),
+        Text(
+          ' 填寫完成將解鎖黑色主題哦！',
+          style: TextStyle(color: Colors.grey.shade800, fontSize: 16),
+        ),
+      ],
     ),
     buttons: [
       DialogButton(
